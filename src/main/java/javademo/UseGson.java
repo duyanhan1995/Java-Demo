aa2fc84ffe858e81a94ea2fc7c93d38b5ff13fa8
duyanhan1995 <duyanhan1995@gmail.com>
2018-03-08 14:29:58 +0800

    使用Gson包将---复杂数据类型---转换成通用JSON格式数据

--- src/main/java/javademo/UseGson.java
+++ src/main/java/javademo/UseGson.java
@@ -1,5 +1,29 @@
 package javademo;
 
-public class UseGson {
+import org.junit.Test;
 
+import com.google.gson.Gson;
+
+import javademo.UseGsonLib.Data;
+import javademo.UseGsonLib.Other;
+
+public class UseGson {
+	
+	@Test
+	public void test() {
+		// 第一步，初始化复杂数据类型
+		Data data = new Data();
+		data.setData1("data1");
+		data.setData2("data2");
+			Other other = new Other();
+			other.setOther1("other1");
+			other.setOther2("other2");
+		data.setOther(other);
+		
+		// 第二步，创建Gson对象
+		Gson gson = new Gson();
+		String json = gson.toJson(data);
+		System.out.println(json);
+		
+	}
 }
