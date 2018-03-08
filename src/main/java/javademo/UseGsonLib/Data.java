27c913594d3d9743cd6f5ca5972e6a620e488b0a
duyanhan1995 <duyanhan1995@gmail.com>
2018-03-08 14:28:59 +0800

    UseGson示例的引用文件

--- src/main/java/javademo/UseGsonLib/Data.java
+++ src/main/java/javademo/UseGsonLib/Data.java
@@ -2,4 +2,31 @@ package javademo.UseGsonLib;
 
 public class Data {
 
+	String data1;
+	String data2;
+	Other other;
+	public String getData1() {
+		return data1;
+	}
+	public void setData1(String data1) {
+		this.data1 = data1;
+	}
+	public String getData2() {
+		return data2;
+	}
+	public void setData2(String data2) {
+		this.data2 = data2;
+	}
+	public Other getOther() {
+		return other;
+	}
+	public void setOther(Other other) {
+		this.other = other;
+	}
+	@Override
+	public String toString() {
+		return "Data [data1=" + data1 + ", data2=" + data2 + ", other=" + other + "]";
+	}
+	
+	
 }
--- src/main/java/javademo/UseGsonLib/Other.java
+++ src/main/java/javademo/UseGsonLib/Other.java
@@ -2,4 +2,24 @@ package javademo.UseGsonLib;
 
 public class Other {
 
+	String other1;
+	String other2;
+	public String getOther1() {
+		return other1;
+	}
+	public void setOther1(String other1) {
+		this.other1 = other1;
+	}
+	public String getOther2() {
+		return other2;
+	}
+	public void setOther2(String other2) {
+		this.other2 = other2;
+	}
+	@Override
+	public String toString() {
+		return "Other [other1=" + other1 + ", other2=" + other2 + "]";
+	}
+	
+	
 }
